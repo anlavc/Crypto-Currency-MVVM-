139526f90feb138d3c3f8f47f5598a7a58cfe0e3
Anıl Avcı <anilavci52@gmail.com>
2023-02-02 13:22:50 +0300

    fix

---
+++ CryptoApp/eXTENSİONS/StringFormatter + Ext.swift
@@ -0,0 +1,24 @@
+//
+//  StringFormatter + Ext.swift
+//  CryptoApp
+//
+//  Created by Anıl AVCI on 29.01.2023.
+//
+
+import Foundation
+
+extension String {
+    // formatting text for currency textField
+    func currencyFormatting() -> String {
+        if let value = Double(self) {
+            let formatter = NumberFormatter()
+            //formatter.numberStyle = .currency
+            formatter.maximumFractionDigits = 2
+            formatter.minimumFractionDigits = 2
+            if let str = formatter.string(for: value) {
+                return str
+            }
+        }
+        return ""
+    }
+}
